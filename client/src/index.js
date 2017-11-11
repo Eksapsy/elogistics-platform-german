72462709b4960a66d8bf40500fbf7864b27392e4
eksapsy <p16anas@ionio.gr>
2017-11-11 13:57:52 +0200

    Fix Crash

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,8 @@ import App from './components/App';
 import configureStore from './store/storeConfigurations';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log('%cApp Version 1.1.2\n%cDo not attempt to do anything in the console.\t\n%cContact with the developer %capostolis.anastasiou.alpha@gmail.com\t', 'background-color: #212121; color: #1ab7ea; font-size: 28px', 'background-color: #212121; color: #bd081c; font-size: 16px', 'background-color: #212121; color: #1ab7ea; font-size: 12px', 'background-color: #212121; color: #1ab7ea; font-weight: 900; font-size: 12px');
+
+console.log('%cApp Version 1.2.0\n%cDo not attempt to do anything in the console.\t\n%cContact with the developer %capostolis.anastasiou.alpha@gmail.com\t', 'background-color: #212121; color: #1ab7ea; font-size: 28px', 'background-color: #212121; color: #bd081c; font-size: 16px', 'background-color: #212121; color: #1ab7ea; font-size: 12px', 'background-color: #212121; color: #1ab7ea; font-weight: 900; font-size: 12px');
 
 const store = configureStore();
 
