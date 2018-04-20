823a0c4296e35f92e2998356f3ad2b32a54b3a5c
Eksapsy <michael.tolis@gmail.com>
2018-04-20 13:39:19 +0300

    git ignore

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Table, Icon, Form} from 'semantic-ui-react';
 import './index.css';
 
 export default class ProductList extends Component {
@@ -9,7 +10,360 @@ export default class ProductList extends Component {
   render() {
     return (
       <div className='productList'>
-        <p>aha</p>
+        <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>ID</Table.HeaderCell>
+        <Table.HeaderCell>PRODUCT</Table.HeaderCell>
+        <Table.HeaderCell>AMOUNT</Table.HeaderCell>
+        <Table.HeaderCell>ORDER</Table.HeaderCell>
+        <Table.HeaderCell>RETAIL</Table.HeaderCell>
+        <Table.HeaderCell>WHOLESALE</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row positive>
+        <Table.Cell>Jimmy</Table.Cell>
+        <Table.Cell>
+          <Icon name='checkmark' />
+            Approved
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Jamie</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row negative>
+        <Table.Cell>Jill</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>Unknown</Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="AMOUNT"/>
+        </Table.Cell>
+        <Table.Cell>
+          <Form.Input placeholder="ORDER"/>
+        </Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+        <Table.Cell>No Name Specified</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
       </div>
     )
   }
