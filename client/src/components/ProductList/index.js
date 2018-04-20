e518c805639719983f2dd28ce288a6d1bade150b
Eksapsy <michael.tolis@gmail.com>
2018-04-20 12:51:28 +0300

    semantic

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import ProductRow from './ProductRow';
 import './index.css';
 
 export default class ProductList extends Component {
   constructor(props) {
     super(props);
-    this.productRows = this.productRows.bind(this);
   }
-  productRows() {
-  let rows = [<ProductRow />, <ProductRow />];
-    
-    return (
-      rows.map((row, index) => {
-        return (
-          row
-        );
-      })  
-    );
-  }
+
   render() {
     return (
       <div className='productList'>
-        {this.productRows()}
+        <p>aha</p>
       </div>
     )
   }
