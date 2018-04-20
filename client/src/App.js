0324cacb2298a5e79a168f9a11032b41aedde9b2
Eksapsy <michael.tolis@gmail.com>
2018-04-20 12:49:33 +0300

    semantic

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MainScene from './scenes/MainScene';
 import Footer from './components/Footer';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 class App extends Component {
